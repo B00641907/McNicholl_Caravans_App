e86ac9d44efb2cdb5ee6a5ff2e1e2762b00ace0e
Eoghan Wilkinson <Wilkinson-E4email.ulster.ac.uk>
2018-10-18 13:16:39 +0100

    Linking pages; Needs fixing

---
+++ app/src/main/java/com/example/b00641907/mcnicholl_caravans_app/Login_Register.java
@@ -0,0 +1,18 @@
+package com.example.b00641907.mcnicholl_caravans_app;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Login_Register extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.Login_Register, container, false);
+    }
+}
--- app/src/main/java/com/example/b00641907/mcnicholl_caravans_app/MainActivity.java
+++ app/src/main/java/com/example/b00641907/mcnicholl_caravans_app/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.b00641907.mcnicholl_caravans_app;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -19,12 +22,51 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.nav_drawer_open, R.string.nav_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        if (savedInstanceState ==null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new MessageFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_message);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+            case R.id.nav_about_us:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AboutUs()).commit();
+                break;
+            case R.id.nav_holiday_homes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Holiday_Homes()).commit();
+                break;
+            case R.id.nav_login_register:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Login_Register()).commit();
+                break;
+            case R.id.nav_maps:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Maps()).commit();
+                break;
+            case R.id.nav_park_information:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Park_Information()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
